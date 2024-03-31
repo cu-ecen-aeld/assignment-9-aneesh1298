bbd4eb770743418400943cdb5297ac62e7355a31
aneesh1298 <angu1298@colorado.edu>
2024-03-30 23:32:19 -0600

    changes

--- base_external/package/aesdchar_driver/aesdchar_driver.mk
+++ base_external/package/aesdchar_driver/aesdchar_driver.mk
@@ -8,7 +8,6 @@
 AESDCHAR_DRIVER_VERSION = 'c4854763bddd87a78fd46b33d3b8d73c6a78a636'
 #AESDCHAR_DRIVER_VERSION = 'ab8df669a89cf2a89645ebc99ea2d10a186e0a96'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
-# with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 AESDCHAR_DRIVER_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-aneesh1298.git'
 AESDCHAR_DRIVER_SITE_METHOD = git
